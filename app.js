7735cefbbfae2c6f3e130ecd314b0a24547f023e
Noah Becker <102703954+noah35becker@users.noreply.github.com>
2022-07-20 01:10:13 -0400

    generate html file

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 
+const fs = require('fs');
+const generatePage = require('./src/page-template');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const printProfileData = profileDataArr => 
-    profileDataArr.forEach(profileItem => console.log(profileItem));
+const [theirName, theirGithub] = profileDataArgs;
 
 
-printProfileData(profileDataArgs);
+fs.writeFile('index.html', generatePage(theirName, theirGithub), err => {
+    if (err)
+        throw err;
+    
+    console.log('Portfolio complete! Check out index.html to see the output!');
+});
